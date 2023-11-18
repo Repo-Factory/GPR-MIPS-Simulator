adeb11b64316fee76f3ed52b1fe1bb51237e0d4d
Conner Sommerfield <conner.sommerfield@gmail.com>
2023-11-17 16:15:07 -0800

    Add documentation to pipeline

--- gprSim/src/processor/pipeline.cpp
+++ gprSim/src/processor/pipeline.cpp
@@ -1,8 +1,10 @@
 /* 
- * @brief 
+ * @brief Define pipeline stages for CPU
  *
- * 
+ * CPU instructions won't be executed in one cycle but instead will be simulated to take place in a 5-stage pipelined CPU 
+ * This will consist of IF,ID,EX,MEM,WB stages which will take place concurrently
  */
+
 #include "cpu.hpp"
 #include "pipeline.hpp"
 #include "instructions.hpp"
@@ -18,6 +20,7 @@
 #define INCREMENT 1
 #define NUM_STAGES 5
 
+// This jump table will help us easily call each stage of pipeline
 std::function<void(MIPSCPU&)> stage_v_table[NUM_STAGES] =
 {
      &IF_STAGE,
@@ -32,6 +35,9 @@ Stage incrementStage(const Stage currentStage)
      return static_cast<Stage>(static_cast<int>(currentStage) + 1);
 }
 
+// We will use a dequeue to simulate the concurrency of the stages. Every new instruction introduced to the queue will start in the IF stage, and move through the stages 
+// until it reaches WB where it will then be removed. This means the pipeline will have at most 5 instructions at one time, which will be executed in order based
+// on the way they came. The execution will be implemented by calling the function at the index of the v_table corresponding to current stage (0-4)
 void clockCycle(MIPSCPU& cpu) 
 {
      cpu.pipeline.push_back(Stage::IF_STAGE);
@@ -46,17 +52,21 @@ void clockCycle(MIPSCPU& cpu)
      }
 }
 
+// Simple fetch stage which reads instruction at current program counter
 void IF_STAGE(MIPSCPU& cpu) 
 {
      cpu.latches.IF_ID_LATCH = readContents(cpu.pc++);
 }
 
+// Decode stage will split instruction into corresponding tokens and store it in the appropriate buffer. We will use a tagged union to transfer information
+// on what kind of instruction is seen through the buffer
 void ID_STAGE(MIPSCPU& cpu) 
 {
      const int32_t instruction = cpu.latches.IF_ID_LATCH;
      const int32_t opcode = BinaryParser::extractOpcode(instruction);
      const Instruction instructionTag = static_cast<Instruction>(opcode);
 
+     // Switch case will fill the union with the appropriate instruction response type (depending on number and type of tokens)
      switch(opcode)
      {
           case NOP_OPCODE():
@@ -135,11 +145,13 @@ void ID_STAGE(MIPSCPU& cpu)
                break;
      }
 
+     // Make sure to update CPU usage statistics
      cpu.cyclesPerUnit(SET, cyclesTableClosure()()[opcode].data()); // Increment cycles of each functional unit
      cpu.instructionsExecuted(INCREMENT);      // Incremenent total instructions executed
      cpu.nopCount(opcode==NOP_OPCODE());
 }
 
+// Here we will call our corresponding opcode functions that will perform the appropriate arithmetic and store in the next latch
 void EX_STAGE(MIPSCPU& cpu) 
 {
      switch(cpu.latches.ID_EX_LATCH.type)
@@ -180,6 +192,7 @@ void EX_STAGE(MIPSCPU& cpu)
      }              
 }
 
+// Here we will write to memory if necessary for our load instructions
 void MEM_STAGE(MIPSCPU& cpu) 
 {
      switch(cpu.latches.EX_MEM_LATCH.type)
@@ -204,6 +217,7 @@ void MEM_STAGE(MIPSCPU& cpu)
      }
 }
 
+// Simple register writeback stage for R-Type Instructions
 void WB_STAGE(MIPSCPU& cpu) 
 {
      if (cpu.latches.MEM_WB_LATCH.destination != NULL)
